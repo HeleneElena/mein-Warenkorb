3eb51e34c6ad244ed649d2afec1c28241b1c94c0
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-03-22 17:43:41 +0100

    Add files via upload

---
+++ Warenkorb/js/calcPrice.js
@@ -0,0 +1,23 @@
+
+function calcPrice() {
+    const cartWrapper = document.querySelector('.cart-wrapper'); // mein Korb
+    const warenArr = cartWrapper.querySelectorAll('[data-id]'); // Waren im Korb
+    const gesamtSumme = document.querySelector('.total-price'); 
+
+    let totalPrice = 0;
+
+    warenArr.forEach((item) => {
+        console.log(item);
+
+        const mengeWare = item.querySelector('[data-counter]').innerText;
+        const priceWare = item.querySelector('.price__currency .value').innerText; 
+
+        console.log(mengeWare);
+        console.log(priceWare);
+
+        totalPrice += parseInt(mengeWare) * parseInt(priceWare);
+        console.log(totalPrice);
+        
+    })
+    gesamtSumme.innerText = totalPrice;
+}
